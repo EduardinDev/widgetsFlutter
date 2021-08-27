cb3e1b31651427653f28fe915a02b1fb13967433
Eduardo Olivo <keolivor@utn.edu.ec>
2021-08-27 08:55:11 -0500

    refactorización del draggable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:draggablesheet/pages/home_page.dart';
+import 'package:draggablesheet/pages/draggableSheet.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage( )
+      home: DraggableWidget()
     );
   }
 }
