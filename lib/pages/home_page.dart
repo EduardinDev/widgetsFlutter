87d5def0a0a28e99674416dd00ef65265129fbde
Eduardo Olivo <keolivor@utn.edu.ec>
2021-08-27 08:51:26 -0500

    draggable-sheet

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Draggable Scrollable'),
+          elevation: 0,
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: Text(
+                'Hola mundo',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            BottomModal()
+          ],
+        ));
+  }
+}
+
+class BottomModal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.1,
+      minChildSize: 0.1,
+      maxChildSize: 0.7,
+      builder: (BuildContext context, ScrollController scrollController) {
+        return Container(
+          child: ListaItems(scrollController),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        );
+      },
+    );
+  }
+}
+
+class ListaItems extends StatelessWidget {
+  final ScrollController scrollController;
+  final List items = []..length = 20;
+
+  ListaItems(this.scrollController);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: this.scrollController,
+        itemCount: items.length,
+        itemBuilder: (context, index) => ListTile(
+              title: Text('Item: $index'),
+            ));
+  }
+}
